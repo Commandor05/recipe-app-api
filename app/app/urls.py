fcbb444b27c08ea42eb8c0c61e5a1fa6d3af9b73
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2026-04-15 16:45:06 +0300

    feat(user): build user api

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+    ),
+    path("api/user/", include("user.urls")),
 ]
